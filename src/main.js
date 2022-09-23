a2d473b49e8e9648fb4ca901638391776326f016
Matthias Fisch <mail@matthias-fisch.de>
2022-09-24 00:04:07 +0200

    Fix styling of components

--- src/main.js
+++ src/main.js
@@ -1,19 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { BootstrapVue } from 'bootstrap-vue'
+import { BootstrapVue, FormInputPlugin, FormSelectPlugin } from 'bootstrap-vue'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { createPinia, PiniaVuePlugin } from 'pinia'
 import VueCompositionAPI from '@vue/composition-api'
+
+Vue.use(FormInputPlugin)
+Vue.use(FormSelectPlugin)
+Vue.use(BootstrapVue)
+
+import './app.css'
 
 Vue.use(PiniaVuePlugin)
 const pinia = createPinia()
 Vue.use(VueCompositionAPI)
 
 Vue.config.productionTip = false
-
-Vue.use(BootstrapVue)
 
 new Vue({
   pinia,
