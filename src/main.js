b4cd092e01b7b900fde508310d19f0d671267a3f
Matthias Fisch <mail@matthias-fisch.de>
2022-09-23 19:49:27 +0200

    Remove pinia

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue } from 'bootstrap-vue'
-import { createPinia } from 'pinia'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -9,7 +8,6 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
-Vue.use(createPinia())
 
 new Vue({
   render: h => h(App),
